3942c58d004cb122029b5d2ce360eb06fae89998
Alexey <alexey.anasov@yandex.by>
2024-08-24 20:56:43 +0300

    слайд-шоу на jquery

---
+++ js/slideshow.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  let slideIndex = 0;
+  showPhotos();
+
+  function showPhotos() {
+    let i;
+    const slides = $(".photo img");
+
+    for (i = 0; i < slides.length; i++) {
+      $(slides[i]).hide();
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+
+    $(slides[slideIndex - 1]).show();
+
+    setTimeout(showPhotos, 300);
+  }
+});
